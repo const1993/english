cc331bcf8df4ac74399f7065b62df536256f9ca4
const1993 <const1993@gmail.com>
2016-11-03 09:38:36 +0300

    Add scrollspy

--- webapp/js/modules/base.js
+++ webapp/js/modules/base.js
@@ -2,14 +2,14 @@ angular.module('anchorScrollOffsetExample', [])
     .run(['$anchorScroll', function($anchorScroll) {
         $anchorScroll.yOffset = 50;   // always scroll by 50 extra pixels
     }])
-    .controller('headerCtrl', ['$anchorScroll', '$location', '$scope',
+    .controller('changing', ['$anchorScroll', '$location', '$scope',
         function($anchorScroll, $location, $scope) {
             $scope.gotoAnchor = function(x) {
-                var newHash = 'anchor' + x;
+                var newHash = x;
                 if ($location.hash() !== newHash) {
                     // set the $location.hash to `newHash` and
                     // $anchorScroll will automatically scroll to it
-                    $location.hash('anchor' + x);
+                    $location.hash(x);
                 } else {
                     // call $anchorScroll() explicitly,
                     // since $location.hash hasn't changed
